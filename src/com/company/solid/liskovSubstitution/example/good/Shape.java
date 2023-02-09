459fab68f5d94ef11059b2de4b3733e29d5a2b2f
Hilal2013 <hilalozkul@gmail.com>
2023-02-08 22:26:18 -0500

    added liskovSubstitution package

---
+++ src/com/company/solid/liskovSubstitution/example/good/Shape.java
@@ -0,0 +1,7 @@
+package com.company.solid.liskovSubstitution.example.good;
+
+public interface Shape {
+
+    int getArea();
+
+}
--- src/com/company/solid/openClosed/example2/good/Calculator.java
+++ src/com/company/solid/openClosed/example2/good/Calculator.java
@@ -8,7 +8,9 @@ public class Calculator {
         int num2 = 2;
 
         operation.calculate(num1, num2);
-
+//// What happens if I want to add another operation later?   % modulus
+        // all i need to just create modulus class and implement interface. Am I changing in the existing classes
+        //to be able to do that//im not changing//I cannot break this one
     }
 
 }
