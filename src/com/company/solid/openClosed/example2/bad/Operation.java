ef31a5d08cdd093602df6b82728b5a06c3263e1a
Hilal2013 <hilalozkul@gmail.com>
2023-02-08 21:25:19 -0500

    added openClosed package

---
+++ src/com/company/solid/openClosed/example2/bad/Operation.java
@@ -0,0 +1,9 @@
+package com.company.solid.openClosed.example2.bad;
+
+public class Operation {
+    private OperationType type;
+
+    public OperationType getType() {
+        return type;
+    }
+}
---
+++ src/com/company/solid/openClosed/example2/good/Operation.java
@@ -0,0 +1,7 @@
+package com.company.solid.openClosed.example2.good;
+
+public interface Operation {
+
+    void calculate(int num1, int num2);
+
+}
